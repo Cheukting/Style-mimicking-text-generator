e104132361406800faf196b7cfa322b5689fe434
Cheuk Ho <cheukting.ho@gmail.com>
2018-02-22 15:40:29 +0000

    Add more resources to train the network.

--- text-generator.py
+++ text-generator.py
@@ -1,18 +1,27 @@
-
-
 import keras
 import numpy as np
 import re
+import os
 
 # this is used to remove stage direction if we don't want them
 def remove_stage_dir(text):
     text = re.sub("[\<].*?[\>]", "", text)
     text = re.sub("\\s+", " ", text)
     return text
+def remove_SPEECH(text):
+    text = re.sub("SPEECH \d+", "", text)
+    text = re.sub("\\s+", " ", text)
+    return text
 
+path = './path'
+in_sentences=[]
+# read all files in the floder (need to be txt with UTF-8 encoding)
 # chop it up in sentances (for Tokenizer)
-text = ''.join(open("hamlet.txt", encoding = "UTF-8").readlines())
-in_sentences = re.split(r' *[\.\?!][\'"\)\]]* *', remove_stage_dir(text))
+for filename in os.listdir(path):
+    text = ''.join(open(path+'/'+filename, encoding = "UTF-8", mode="r").readlines())
+    split_text = re.split(r' *[\.\?!][\'"\)\]]* *', remove_stage_dir(text))
+    for chunk in split_text:
+        in_sentences.append(chunk)
 
 
 print(in_sentences[0:10])
@@ -72,9 +81,8 @@ for i in range (0,len(sentences)):
 from keras import layers
 
 model = keras.models.Sequential()
-model.add(layers.Embedding(max_num_word, 128, input_length=maxlen))
-model.add(layers.LSTM(128,return_sequences=True))
-model.add(layers.Flatten())
+model.add(layers.Embedding(max_num_word, 256, input_length=maxlen))
+model.add(layers.LSTM(256))
 model.add(layers.Dense(max_num_word, activation='softmax'))
 
 model.summary()
@@ -84,14 +92,14 @@ optimizer = keras.optimizers.RMSprop(lr=0.01)
 model.compile(loss='mse', optimizer=optimizer)
 
 #this is for sampling the next work with a prediction distribution
-def sample(preds, temperature=1.0):
+def sample(preds, temperature=0.1):
     preds = np.asarray(preds).astype('float64')
     exp_preds = preds - np.exp(temperature)
     preds = exp_preds / np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-model.fit(x, y, batch_size=128, epochs=1)
+model.fit(x, y, batch_size=256, epochs=1)
 
 #to change back to word
 reverse_word_map = dict(map(reversed, tokenizer.word_index.items()))
@@ -105,7 +113,7 @@ generated_seed = token_word[start_index: start_index + maxlen]
 generated_text = ' '.join([reverse_word_map.get(i) for i in generated_seed])
 print('--- Generating with seed ---')
 print(generated_text)
-print('--- --- --- --- ---')
+print('--- --- --- --- --- ---')
 
 for i in range(20): #generate 20 words
 
@@ -122,4 +130,4 @@ for i in range(20): #generate 20 words
 
 print('--- Generated text ---')
 print(generated_text)
-print('--- --- --- --- ---')
+print('--- --- --- --- --- ---')
